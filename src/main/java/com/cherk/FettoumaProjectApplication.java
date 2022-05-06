e60f6ed321b2f5bcf79bfed2a6d174cc777633e1
Ouma1993 <cherkaouifettouma@gmail.com>
2022-05-06 14:30:19 +0000

    first commit

---
+++ src/main/java/com/cherk/FettoumaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cherk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FettoumaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FettoumaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherk/FettoumaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FettoumaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
